51af46faa9f507392c867b30df7e0912d80866f1
Revanth Revoori <rrevoori@rippling.com>
2024-09-18 13:44:37 -0700

    update run migration files script

--- nara_backend/scripts/run_migrations.sh
+++ nara_backend/scripts/run_migrations.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
+# copying env file
+ENV_FILE_DIR=$(dirname "$BASH_SOURCE")/../
+if [[ ! -e ${ENV_FILE_DIR}/.env ]]; then
+    cp ${ENV_FILE_DIR}/.env.sample ${ENV_FILE_DIR}/.env
+fi
+
 echo "Running Database Migrations"
-python ../manage.py makemigrations
-python ../manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 
 exec "$@"
