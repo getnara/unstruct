e2548f1a6caea61cf5ff67fb562fc6cd1fdd9bfe
Nikhil Aourpally <nikhil@faircompute.com>
2024-10-03 23:11:26 -0700

    Update the action model (#13)

--- nara_backend/apps/core/models/action.py
+++ nara_backend/apps/core/models/action.py
@@ -3,16 +3,20 @@ from django.utils.translation import gettext_lazy as _
 
 from apps.common.models import NBaseWithOwnerModel
 
-
 class ACTION_TYPE(models.TextChoices):
     EXTRACTION = "EXTRACT", _("Extraction")
     GENERATION = "GENERATE", _("Generation")
 
+class OUTPUT_COLUMN_TYPE(models.TextChoices):
+    TEXT = "TEXT", _("Text")
+    NUMBER = "NUMBER", _("Number")
+    DATE = "DATE", _("Date")
 
 class Action(NBaseWithOwnerModel):
-    name = models.CharField(max_length=200)
-    description = models.TextField()
+    output_column_name = models.CharField(max_length=200)
+    output_column_type = models.CharField(max_length=50, choices=OUTPUT_COLUMN_TYPE, default=OUTPUT_COLUMN_TYPE.TEXT)
     action_type = models.CharField(max_length=200, choices=ACTION_TYPE, default=ACTION_TYPE.EXTRACTION)
+    description = models.TextField()
 
     def __str__(self):
-        return self.name
+        return self.output_column_name
